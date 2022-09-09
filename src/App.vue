6a79b5b76519890a3fa7f393af0a227d71641efa
David Freitas <davidfreitas@MacBook-Pro-de-David.local>
2022-09-09 07:10:52 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <NavBar />
+  
+  <RouterView />
+</template>
+
+<script setup>
+  import NavBar from '@/components/layout/NavBar.vue'
+</script>
---
+++ src/components/layout/NavBar.vue
@@ -0,0 +1,13 @@
+<template>    
+    <nav class="navbar px-2 py-3 bg-blue-500">
+        <div class="container flex flex-wrap justify-between items-center mx-auto">
+            <a href="#" class="flex items-center">
+                <span class="self-center text-xl font-semibold whitespace-nowrap text-white">Noteballs</span>
+            </a>
+
+            <div class="w-auto" id="navbar-default">
+                <RouterLink to="/" class="block bg-transparent text-white">Notes</RouterLink>
+            </div>
+        </div>
+    </nav>
+</template>

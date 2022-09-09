1366b9469d7e288f4e07cc7748dcb58565684691
David Freitas <davidfreitas.dev@outlook.com>
2022-09-09 08:38:52 -0300

    navbar and font style

--- src/components/layout/NavBar.vue
+++ src/components/layout/NavBar.vue
@@ -1,12 +1,14 @@
 <template>    
-    <nav class="navbar px-2 py-3 bg-blue-500">
+    <nav class="navbar p-3 bg-blue-500">
         <div class="container flex flex-wrap justify-between items-center mx-auto">
             <a href="#" class="flex items-center">
                 <span class="self-center text-xl font-semibold whitespace-nowrap text-white">Noteballs</span>
             </a>
 
-            <div class="w-auto" id="navbar-default">
-                <RouterLink to="/" class="block bg-transparent text-white">Notes</RouterLink>
+            <div class="w-auto">
+                <button class="w-10 h-10 rounded-full text-2xl text-white bg-blue-400 ">
+                    +
+                </button>
             </div>
         </div>
     </nav>
